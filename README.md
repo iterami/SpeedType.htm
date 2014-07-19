d36b159ef3a9f5aa650208b52dcbac28105e5105
Jan Horava <czechinthemail@gmail.com>
2014-07-18 21:36:42 -0700

    added project files

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-Speedtype.htm
-=============
+Type the letters that appear and press `enter` to gain a point! You start with 10 seconds to type the letters. Each time you earn a point, you have 0.1 less seconds to type the next set of letters.
---
+++ js/speedtype.js
@@ -0,0 +1,103 @@
+function decisecond(){
+    document.getElementById('time').innerHTML =
+      (parseFloat(document.getElementById('time').innerHTML) - .1).toFixed(1);
+
+    if(parseFloat(document.getElementById('time').innerHTML) <= 0){
+        stop();
+    }
+}
+
+function enter(){
+    if(document.getElementById('text').value == document.getElementById('target').innerHTML){
+        document.getElementById('text').value='';
+
+        document.getElementById('score').innerHTML =
+          parseInt(document.getElementById('score').innerHTML) + 1;
+
+        document.getElementById('target').innerHTML=
+          random_letter()
+          + random_letter()
+          + random_letter()
+          + random_letter()
+          + random_letter();
+
+        time_remaining -= .1;
+        document.getElementById('time').innerHTML = time_remaining.toFixed(1);
+    }
+}
+
+function random_letter(){
+    return 'abcdefghijklmnopqrstuvwxyz'[Math.floor(Math.random() * 26)]
+}
+
+function start(){
+    clearInterval(interval);
+
+    time_remaining = 10.0;
+
+    document.getElementById('target').innerHTML=
+      random_letter()
+      + random_letter()
+      + random_letter()
+      + random_letter()
+      + random_letter();
+
+    document.getElementById('text').value = '';
+    document.getElementById('text').disabled = false;
+    document.getElementById('text').focus();
+
+    document.getElementById('time').innerHTML = time_remaining;
+
+    document.getElementById('score').innerHTML = 0;
+
+    document.getElementById('start-button').value = 'Stop [ESC]';
+    document.getElementById('start-button').onclick = function(){
+        stop();
+    };
+
+    interval = setInterval(
+      'decisecond()',
+      100
+    );
+}
+
+function stop(){
+    clearInterval(interval);
+
+    document.getElementById('text').disabled = true;
+    interval = 0;
+
+    document.getElementById('start-button').value = 'Start [H]';
+    document.getElementById('start-button').onclick = function(){
+        start();
+    };
+}
+
+var interval = 0;
+var time_remaining = 0;
+
+window.onkeydown = function(e){
+    var key = window.event ? event : e;
+    key = key.charCode ? key.charCode : key.keyCode;
+
+    // Enter
+    if(key == 13){
+        enter();
+
+    // ESC
+    }else if(key == 27){
+        stop();
+    }
+};
+
+window.onkeyup = function(e){
+    // Doesn't work if the game is running.
+    if(interval == 0){
+        var key = window.event ? event : e;
+        key = key.charCode ? key.charCode : key.keyCode
+
+        if(key == 72){
+            start();
+        }
+    }
+};
