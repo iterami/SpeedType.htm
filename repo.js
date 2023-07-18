1d9d57bbae5d98569465fe61a2fd32c900e02390
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 15:03:29 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -46,6 +46,59 @@ function enter(){
     set_time_remaining(time_remaining - core_storage_data['time-decrease']);
 }
 
+function repo_escape(){
+    if(!core_intervals['interval']
+      && !core_menu_open){
+        start();
+    }
+}
+
+function repo_init(){
+    core_repo_init({
+      'events': {
+        'start-button': {
+          'onclick': function(){
+              core_escape();
+              start();
+          },
+        },
+      },
+      'globals': {
+        'letters': 'abcdefghijklmnopqrstuvwxyz',
+        'time': 0,
+        'time_remaining': 0,
+      },
+      'info': '<input id=start-button type=button value=Restart>',
+      'keybinds': {
+        13: {
+          'todo': enter,
+        },
+      },
+      'menu': true,
+      'storage': {
+        'length': 5,
+        'time-decrease': .1,
+        'time-max': 10,
+      },
+      'storage-menu': '<table><tr><td><input class=mini id=length min=1 step=any type=number><td>Length'
+        + '<tr><td><input class=mini id=time-decrease step=any type=number><td>Time Decrease'
+        + '<tr><td><input class=mini id=time-max step=any type=number><td>Time Max</table>',
+      'title': 'SpeedType.htm',
+    });
+    audio_create({
+      'audios': {
+        'boop': {
+          'duration': .1,
+        },
+      },
+    });
+    core_html_store({
+      'ids': [
+        'time',
+      ],
+    });
+}
+
 function set_time_remaining(new_time_remaining){
     time_remaining = new_time_remaining;
     time = time_remaining;
