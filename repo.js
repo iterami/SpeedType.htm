8b7e2efce54d62986f958e3c68c801d5e1598969
Jan Hořava <czechinthemail@gmail.com>
2024-06-01 13:54:22 -0700

    used core_ui_update() for score, target, time, and text

--- repo.js
+++ repo.js
@@ -6,14 +6,18 @@ function decisecond(){
       'number': time - .1,
     });
 
-    core_elements['time'].textContent = core_number_format({
-      'decimals-min': 1,
-      'number': time,
+    core_ui_update({
+      'ids': {
+        'time': core_number_format({
+          'decimals-min': 1,
+          'number': time,
+        }),
+      },
     });
 
     if(time <= 0){
         core_interval_pause_all();
-        document.getElementById('text').readOnly = true;
+        core_elements['text'].readOnly = true;
     }
 }
 
@@ -22,24 +26,25 @@ function enter(){
         start();
     }
 
-    const targetelement = document.getElementById('target');
-    const textelement = document.getElementById('text');
-    if(textelement.value !== targetelement.textContent){
+    if(core_elements['text'].value !== core_elements['target'].textContent){
         return;
     }
 
     audio_start('boop');
 
-    const element = document.getElementById('score');
-    element.textContent = Number.parseInt(
-      element.textContent,
-      10
-    ) + 1;
-    targetelement.textContent = core_random_string({
-      'characters': letters,
-      'length': core_storage_data['length'],
+    core_ui_update({
+      'ids': {
+        'score': Number.parseInt(
+          core_elements['score'].textContent,
+          10
+        ) + 1,
+        'target': core_random_string({
+          'characters': letters,
+          'length': core_storage_data['length'],
+        }),
+      },
     });
-    textelement.value = '';
+    core_elements['text'].value = '';
 
     set_time_remaining(time_remaining - core_storage_data['time-decrease']);
 }
@@ -83,30 +88,36 @@ function repo_init(){
         + '<tr><td><input class=mini id=time-max step=any type=number><td>Time Max</table>',
       'title': 'SpeedType.htm',
     });
-    core_elements['time'] = document.getElementById('time');
 }
 
 function set_time_remaining(new_time_remaining){
     time_remaining = new_time_remaining;
     time = time_remaining;
-    core_elements['time'].textContent = core_round({
-      'decimals': 1,
-      'number': time_remaining,
+    core_ui_update({
+      'ids': {
+        'time': core_round({
+          'decimals': 1,
+          'number': time_remaining,
+        }),
+      },
     });
 }
 
 function start(){
     set_time_remaining(core_storage_data['time-max']);
 
-    document.getElementById('score').textContent = 0;
-    document.getElementById('target').textContent = core_random_string({
-      'characters': letters,
-      'length': core_storage_data['length'],
+    core_ui_update({
+      'ids': {
+        'score': 0,
+        'target': core_random_string({
+          'characters': letters,
+          'length': core_storage_data['length'],
+        }),
+        'text': '',
+      },
     });
-    const element = document.getElementById('text');
-    element.value = '';
-    element.readOnly = false;
-    element.focus();
+    core_elements['text'].readOnly = false;
+    core_elements['text'].focus();
 
     core_interval_modify({
       'id': 'interval',
