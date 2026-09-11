1970fa41dcd4a94aa336585d4d84e525cbb221b7
Jan Hořava <czechinthemail@gmail.com>
2026-09-11 08:57:25 -0700

    removed beforeunload usage

--- repo.js
+++ repo.js
@@ -61,12 +61,6 @@ function repo_escape(){
 
 function repo_init(){
     core_repo_init({
-      'beforeunload': function(event){
-          if(score !== 0){
-              core_escape(true);
-              event.preventDefault();
-          }
-      },
       'events': {
         'start': {
           'onclick': function(){
